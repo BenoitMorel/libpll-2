05b62dfc0f7e7dc00bb41f9a84ee3d566e1484d0
BenoitMorel <benomorel@gmail.com>
2018-02-06 16:53:35 +0100

    Template partial function wip

--- src/core_partials.cxx
+++ src/core_partials.cxx
@@ -1,8 +1,20 @@
 
 class TemplateAVX {
+public:
+  typedef __m256d reg;
+  static const int vecsize = 4;
+  static constexpr auto load = _mm256_load_pd;
+  static constexpr auto mult = _mm256_mul_pd;
+  static constexpr auto add = _mm256_add_pd; 
+};
+
+class TemplateSSE {
   public:
-   typedef __m256d reg;
-   static const int vecsize = 4;
+   typedef __m128d reg;
+   static const int vecsize = 2;
+  static constexpr auto load = _mm_load_pd;
+  static constexpr auto mult = _mm_mul_pd;
+  static constexpr auto add = _mm_add_pd; 
 
 };
 
@@ -20,132 +32,114 @@ void pll_core_template_update_partial_ii(unsigned int sites,
                                                const unsigned int * right_scaler,
                                                unsigned int attrib)
 {
-  unsigned int i,j,k,n;
-  const double * lmat;
-  const double * rmat;
-
-  const unsigned int STATES_PADDED = (STATES+3) & 0xFFFFFFFC;
-  unsigned int span_padded = STATES_PADDED * rate_cats;
+  const unsigned int STATES_PADDED = (STATES+(VEC::vecsize - 1)) & (0xFFFFFFFF - (VEC::vecsize - 1));
+  const unsigned int span_padded = STATES_PADDED * rate_cats;
 
   /* scaling-related stuff */
   unsigned int scale_mode;  /* 0 = none, 1 = per-site, 2 = per-rate */
-  unsigned int scale_mask;
   unsigned int init_mask;
-  typename VEC::reg v_scale_threshold = _mm256_set1_pd(PLL_SCALE_THRESHOLD);
-  typename VEC::reg v_scale_factor = _mm256_set1_pd(PLL_SCALE_FACTOR);
-
-  if (!parent_scaler)
-  {
-    /* scaling disabled / not required */
-    scale_mode = init_mask = 0;
-  }
-  else
-  {
-    /* determine the scaling mode and init the vars accordingly */
+  if (!parent_scaler) {
+    scale_mode = 0;
+    init_mask = 0;
+  } else {
     scale_mode = (attrib & PLL_ATTRIB_RATE_SCALERS) ? 2 : 1;
     init_mask = (scale_mode == 1) ? 0xF : 0;
     const size_t scaler_size = (scale_mode == 2) ? sites * rate_cats : sites;
-    /* add up the scale vector of the two children if available */
     pll_fill_parent_scaler(scaler_size, parent_scaler, left_scaler, right_scaler);
   }
-
+  
+  typename VEC::reg v_scale_threshold = _mm256_set1_pd(PLL_SCALE_THRESHOLD);
+  typename VEC::reg v_scale_factor = _mm256_set1_pd(PLL_SCALE_FACTOR);
   size_t displacement = (STATES_PADDED - STATES) * (STATES_PADDED);
 
   /* compute CLV */
-  for (n = 0; n < sites; ++n)
+  for (unsigned int n = 0; n < sites; ++n)
   {
-    lmat = left_matrix;
-    rmat = right_matrix;
-    scale_mask = init_mask;
+    const double *lmat = left_matrix;
+    const double *rmat = right_matrix;
+    unsigned int scale_mask = init_mask;
 
-    for (k = 0; k < rate_cats; ++k)
+    for (unsigned int k = 0; k < rate_cats; ++k)
     {
       unsigned int rate_mask = 0xF;
 
       /* iterate over quadruples of rows */
-      for (i = 0; i < STATES_PADDED; i += 4)
+      for (unsigned int i = 0; i < STATES_PADDED; i += VEC::vecsize)
       {
-        typename VEC::reg v_terma0 = _mm256_setzero_pd();
-        typename VEC::reg v_termb0 = _mm256_setzero_pd();
-        typename VEC::reg v_terma1 = _mm256_setzero_pd();
-        typename VEC::reg v_termb1 = _mm256_setzero_pd();
-        typename VEC::reg v_terma2 = _mm256_setzero_pd();
-        typename VEC::reg v_termb2 = _mm256_setzero_pd();
-        typename VEC::reg v_terma3 = _mm256_setzero_pd();
-        typename VEC::reg v_termb3 = _mm256_setzero_pd();
-
+        typename VEC::reg v_terma[VEC::vecsize];
+        typename VEC::reg v_termb[VEC::vecsize];
+        const double * lm[VEC::vecsize];
+        const double * rm[VEC::vecsize];
+        for (unsigned int j = 0; j < VEC::vecsize; ++j) {
+          v_terma[j] = _mm256_setzero_pd();
+          v_termb[j] = _mm256_setzero_pd();
+        }
+        for (unsigned int j = 0; j < VEC::vecsize; ++j) {
+          lm[j]= lmat + j * STATES_PADDED;
+        }
+        for (unsigned int j = 0; j < VEC::vecsize; ++j) {
+          rm[j]= rmat + j * STATES_PADDED;
+        } 
         typename VEC::reg v_mat;
         typename VEC::reg v_lclv;
         typename VEC::reg v_rclv;
 
-        /* point to the four rows of the left matrix */
-        const double * lm0 = lmat;
-        const double * lm1 = lm0 + STATES_PADDED;
-        const double * lm2 = lm1 + STATES_PADDED;
-        const double * lm3 = lm2 + STATES_PADDED;
 
-        /* point to the four rows of the right matrix */
-        const double * rm0 = rmat;
-        const double * rm1 = rm0 + STATES_PADDED;
-        const double * rm2 = rm1 + STATES_PADDED;
-        const double * rm3 = rm2 + STATES_PADDED;
 
         /* iterate over quadruples of columns */
-        for (j = 0; j < STATES_PADDED; j += 4)
+        for (unsigned int j = 0; j < STATES_PADDED; j += VEC::vecsize)
         {
-          v_lclv    = _mm256_load_pd(left_clv+j);
-          v_rclv    = _mm256_load_pd(right_clv+j);
+          v_lclv    = VEC::load(left_clv+j);
+          v_rclv    = VEC::load(right_clv+j);
 
           /* row 0 */
-          v_mat    = _mm256_load_pd(lm0);
-          v_terma0 = _mm256_add_pd(v_terma0,
-                                   _mm256_mul_pd(v_mat,v_lclv));
-          v_mat    = _mm256_load_pd(rm0);
-          v_termb0 = _mm256_add_pd(v_termb0,
-                                   _mm256_mul_pd(v_mat,v_rclv));
-          lm0 += 4;
-          rm0 += 4;
+          v_mat    = VEC::load(lm[0]);
+          v_terma[0] = VEC::add(v_terma[0], VEC::mult(v_mat,v_lclv));
+          v_mat    = VEC::load(rm[0]);
+          v_termb[0] = VEC::add(v_termb[0], VEC::mult(v_mat,v_rclv));
+          lm[0] += VEC::vecsize;
+          rm[0] += VEC::vecsize;
 
           /* row 1 */
-          v_mat    = _mm256_load_pd(lm1);
-          v_terma1 = _mm256_add_pd(v_terma1,
+          v_mat    = _mm256_load_pd(lm[1]);
+          v_terma[1] = _mm256_add_pd(v_terma[1],
                                    _mm256_mul_pd(v_mat,v_lclv));
-          v_mat    = _mm256_load_pd(rm1);
-          v_termb1 = _mm256_add_pd(v_termb1,
+          v_mat    = _mm256_load_pd(rm[1]);
+          v_termb[1] = _mm256_add_pd(v_termb[1],
                                    _mm256_mul_pd(v_mat,v_rclv));
-          lm1 += 4;
-          rm1 += 4;
+          lm[1] += 4;
+          rm[1] += 4;
 
           /* row 2 */
-          v_mat    = _mm256_load_pd(lm2);
-          v_terma2 = _mm256_add_pd(v_terma2,
+          v_mat    = _mm256_load_pd(lm[2]);
+          v_terma[2] = _mm256_add_pd(v_terma[2],
                                    _mm256_mul_pd(v_mat,v_lclv));
-          v_mat    = _mm256_load_pd(rm2);
-          v_termb2 = _mm256_add_pd(v_termb2,
+          v_mat    = _mm256_load_pd(rm[2]);
+          v_termb[2] = _mm256_add_pd(v_termb[2],
                                    _mm256_mul_pd(v_mat,v_rclv));
-          lm2 += 4;
-          rm2 += 4;
+          lm[2] += 4;
+          rm[2] += 4;
 
           /* row 3 */
-          v_mat    = _mm256_load_pd(lm3);
-          v_terma3 = _mm256_add_pd(v_terma3,
+          v_mat    = _mm256_load_pd(lm[3]);
+          v_terma[3] = _mm256_add_pd(v_terma[3],
                                    _mm256_mul_pd(v_mat,v_lclv));
-          v_mat    = _mm256_load_pd(rm3);
-          v_termb3 = _mm256_add_pd(v_termb3,
+          v_mat    = _mm256_load_pd(rm[3]);
+          v_termb[3] = _mm256_add_pd(v_termb[3],
                                    _mm256_mul_pd(v_mat,v_rclv));
-          lm3 += 4;
-          rm3 += 4;
+          lm[3] += 4;
+          rm[3] += 4;
         }
 
         /* point pmatrix to the next four rows */ 
-        lmat = lm3;
-        rmat = rm3;
+        lmat = lm[3];
+        rmat = rm[3];
 
-        typename VEC::reg xmm0 = _mm256_unpackhi_pd(v_terma0,v_terma1);
-        typename VEC::reg xmm1 = _mm256_unpacklo_pd(v_terma0,v_terma1);
+        typename VEC::reg xmm0 = _mm256_unpackhi_pd(v_terma[0],v_terma[1]);
+        typename VEC::reg xmm1 = _mm256_unpacklo_pd(v_terma[0],v_terma[1]);
 
-        typename VEC::reg xmm2 = _mm256_unpackhi_pd(v_terma2,v_terma3);
-        typename VEC::reg xmm3 = _mm256_unpacklo_pd(v_terma2,v_terma3);
+        typename VEC::reg xmm2 = _mm256_unpackhi_pd(v_terma[2],v_terma[3]);
+        typename VEC::reg xmm3 = _mm256_unpacklo_pd(v_terma[2],v_terma[3]);
 
         xmm0 = _mm256_add_pd(xmm0,xmm1);
         xmm1 = _mm256_add_pd(xmm2,xmm3);
@@ -156,13 +150,13 @@ void pll_core_template_update_partial_ii(unsigned int sites,
 
         typename VEC::reg v_terma_sum = _mm256_add_pd(xmm2,xmm3);
 
-        /* compute termb */
+        /* compute term[b] */
 
-        xmm0 = _mm256_unpackhi_pd(v_termb0,v_termb1);
-        xmm1 = _mm256_unpacklo_pd(v_termb0,v_termb1);
+        xmm0 = _mm256_unpackhi_pd(v_termb[0],v_termb[1]);
+        xmm1 = _mm256_unpacklo_pd(v_termb[0],v_termb[1]);
 
-        xmm2 = _mm256_unpackhi_pd(v_termb2,v_termb3);
-        xmm3 = _mm256_unpacklo_pd(v_termb2,v_termb3);
+        xmm2 = _mm256_unpackhi_pd(v_termb[2],v_termb[3]);
+        xmm3 = _mm256_unpacklo_pd(v_termb[2],v_termb[3]);
 
         xmm0 = _mm256_add_pd(xmm0,xmm1);
         xmm1 = _mm256_add_pd(xmm2,xmm3);
@@ -188,7 +182,7 @@ void pll_core_template_update_partial_ii(unsigned int sites,
          * the threshold then scale (all) entries by PLL_SCALE_FACTOR */
         if (rate_mask == 0xF)
         {
-          for (i = 0; i < STATES_PADDED; i += 4)
+          for (unsigned int i = 0; i < STATES_PADDED; i += 4)
           {
             typename VEC::reg v_prod = _mm256_load_pd(parent_clv + i);
             v_prod = _mm256_mul_pd(v_prod, v_scale_factor);
@@ -216,7 +210,7 @@ void pll_core_template_update_partial_ii(unsigned int sites,
     if (scale_mask == 0xF)
     {
       parent_clv -= span_padded;
-      for (i = 0; i < span_padded; i += 4)
+      for (unsigned int i = 0; i < span_padded; i += 4)
       {
         typename VEC::reg v_prod = _mm256_load_pd(parent_clv + i);
         v_prod = _mm256_mul_pd(v_prod,v_scale_factor);
